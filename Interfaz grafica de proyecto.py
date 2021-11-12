b27880f1c78c3c97bfa698204ef7d5622459dee3
GabrielaGonzalez21922 <87957087+GabrielaGonzalez21922@users.noreply.github.com>
2021-11-12 13:49:24 -0600

    Update Interfaz grafica de proyecto.py

--- Interfaz grafica de proyecto.py
+++ Interfaz grafica de proyecto.py
@@ -1,5 +1,5 @@
 import tkinter
-
+#Proporciona un conjunto de herramientas robusto e independiente de la plataforma para administrar ventanas.
 from Usuario import Doctor, Paciente, Usuario, Calificaciones
 from Vista import menu_principal, pedir_datos_loggeo, paciente_encontrado_doctor, elegir_sintoma_usuario, elegir_presupuesto_usuario, dejar_calificacion, elegir_especialidad_doctor, generar_olla_alimenticia_usuario, menu_loggeo_paciente, menu_loggeo_doctor, recaudar_datos_doctor, avisar_nuevo_sintoma, avisar_nuevo_presupuesto, recaudar_datos_paciente, preguntar_tipo_usuario
 from main import menu_doctor, menu_paciente, iniciar_sesion, registrar_usuario, inicio_programa
